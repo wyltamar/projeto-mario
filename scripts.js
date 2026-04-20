691a0ad2fef0f1dd27450a444f674c7db6947bad
wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-04-19 21:53:31 -0300

    Deixando o site responsivo

--- scripts.js
+++ scripts.js
@@ -5,18 +5,10 @@ function mostrarFormulario() {
     
     mostrarMascaraForm()
     formulario.style.left = "40%"
-    formulario.style.transform = "translateX(-17%)"
+    formulario.style.transform = "translateX(-15%)"
 }
 
 function mostrarMascaraForm(){
-    mascaraForm.style.position = "fixed"
-    mascaraForm.style.top = "0"
-    mascaraForm.style.width = "100%"
-    mascaraForm.style.height = "100%"
-    mascaraForm.style.background = `linear-gradient(109deg, 
-                                    rgba(10, 12, 16, 0.99) 15%,
-                                    rgba(10, 12, 16, 0.7) 50%,
-                                    rgba(10, 12, 16, 0.99) 85%)`
     mascaraForm.style.visibility = "visible"
 }
 
